65f856c175caff429cf793605117ff3bc829f786
Your Name <you@example.com>
2025-01-03 21:52:03 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-
 import Navbar from "../components/main-page/navbar/navbar";
+import MainBlock from "../components/main-page/main-block/main-block";
 
 export default function Home() {
   return (
     <main>
       <>
         <Navbar />
+        <MainBlock />
       </>
     </main>
   );
---
+++ src/components/main-page/main-block/main-block.tsx
@@ -0,0 +1,24 @@
+import "./main-block.css";
+
+export default function MainBlock() {
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <div className="logo-wrapper n18">
+          <img className="logo" src="/Logo18.svg" alt="Logo" />
+        </div>
+        <div className="logo-wrapper n19">
+          <img className="logo" src="/Logo19.svg" alt="Logo" />
+        </div>
+        <div className="logo-wrapper n20">
+          <img className="logo" src="/Logo20.svg" alt="Logo" />
+        </div>
+        <div className="logo-wrapper n17">
+          <a href="#">
+            <img src="/Logo17.svg" alt="Logo" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
