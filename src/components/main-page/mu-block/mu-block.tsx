c4cb029edb3a8a8f8b1491a54a85417ff29cfa45
Your Name <you@example.com>
2025-01-04 21:33:35 +0200

    Update

--- src/components/main-page/mu-block/mu-block.tsx
+++ src/components/main-page/mu-block/mu-block.tsx
@@ -11,7 +11,7 @@ export default function Mu() {
             src="/mu.jpg"
             alt="Background"
             layout="fill"
-            objectFit="contain" // Забезпечує збереження пропорцій
+            objectFit="contain"
             objectPosition="center"
             priority
           />
@@ -21,9 +21,9 @@ export default function Mu() {
       <div
         className="absolute pt-[0.7%] pl-[33rem] flex-col pr-[1%] top-0 left-0 w-5/12 h-1/4 bg-white bg-opacity-50 flex"
         style={{
-            paddingLeft: `calc(33rem * ((100vw - 990px) / (1920px - 990px)))`,
-            border: "2px solid red", // Тестова рамка
-          }}
+          paddingLeft: `calc(33rem * ((100vw - 990px) / (1920px - 990px)))`,
+          border: "2px solid red", // Test border
+        }}
       >
         <h2 className="pt-0 font-montserrat mb-2 text-[26px] font-normal text-[#303030] leading-[1.2]">
           Більше <span className="text-white">20 років</span> досвіду
@@ -33,7 +33,6 @@ export default function Mu() {
           Розробка дизайн проектів в приміщеннях різноманітного призначення
         </p>
       </div>
-
       {/* Top Right Block */}
       <div className="absolute pt-[0.7%] flex-col pl-[1%] pr-[24%] top-0 right-0 w-5/12 h-1/4 bg-white bg-opacity-50 flex">
         <h2 className="pt-0 font-montserrat mb-2 text-[26px] font-normal text-[#303030] leading-[1.2]">
@@ -47,7 +46,7 @@ export default function Mu() {
       {/* Bottom Left Block */}
       <div className="absolute pt-[0.7%] flex-col pr-[0.3%] pl-[26%] bottom-0 left-0 w-5/12 h-1/4 bg-white bg-opacity-50 flex">
         <h2 className="pt-0 font-montserrat mb-2 text-[26px] font-normal text-[#303030] leading-[1.2]">
-          Авторський <span className="text-white">супровід </span>об'єкту
+          Авторський <span className="text-white">супровід</span> об'єкту
         </h2>
         <div className="w-[70px] h-[2px] bg-[#f79e1b] my-[5px]"></div>
         <p className="pt-1 font-roboto text-base text-[#555] leading-[1.3]">
@@ -57,7 +56,7 @@ export default function Mu() {
       {/* Bottom Right Block */}
       <div className="absolute pt-[0.7%] flex-col pl-[1%] pr-[26%] bottom-0 right-0 w-5/12 h-1/4 bg-white bg-opacity-50 flex">
         <h2 className="pt-0 font-montserrat mb-2 text-[26px] font-normal text-[#303030] leading-[1.2]">
-          Додатковий <span className="text-white">бонус </span>до дизайн проєкту
+          Додатковий <span className="text-white">бонус</span> до дизайн проєкту
         </h2>
         <div className="w-[70px] h-[2px] bg-[#f79e1b] my-[5px]"></div>
         <p className="pt-1 font-roboto text-base text-[#555] leading-[1.3]">
